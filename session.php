c6129fff1956622feda09b36acd977ab94300ddd
Nirmal Sunny <32394735+nirmalsunny@users.noreply.github.com>
2022-02-23 13:43:04 +0000

    readme edit

--- session.php
+++ session.php
@@ -1,8 +1,8 @@
 <?php
 
-use \nirmalsunny\KlarnaPayments\ApiException;
-
 require_once(__DIR__ . '/vendor/autoload.php');
+
+use \nirmalsunny\KlarnaPayments\ApiException;
 
 $config = \nirmalsunny\KlarnaPayments\Configuration::getDefaultConfiguration();
 $config->setUsername('');
