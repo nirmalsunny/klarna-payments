13eb9535e674587eef8d98f9284a3cdfb7fb6627
Nirmal Sunny <32394735+nirmalsunny@users.noreply.github.com>
2022-02-23 11:16:40 +0000

    Add files via upload

---
+++ lib/KlarnaEndpoint.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Klarna Endpoints
+ *
+ * @category Class
+ * @package  nirmalsunny\KlarnaPayments
+ * @author   Nirmal Sunny
+ * @link     https://github.com/nirmalsunny/klarna-payments
+ */
+
+namespace nirmalsunny\KlarnaPayments;
+
+class KlarnaEndpoint
+{
+    public static $EUROPE = 'https://api.klarna.com';
+    public static $NORTH_AMERICA = 'https://api-na.klarna.com';
+    public static $OCEANIA = 'https://api-oc.klarna.com';
+}
---
+++ lib/KlarnaPlaygroundEndpoint.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Klarna Endpoints
+ *
+ * @category Class
+ * @package  nirmalsunny\KlarnaPayments
+ * @author   Nirmal Sunny
+ * @link     https://github.com/nirmalsunny/klarna-payments
+ */
+
+namespace nirmalsunny\KlarnaPayments;
+
+class KlarnaPlaygroundEndpoint
+{
+    public static $EUROPE = 'https://api.playground.klarna.com';
+    public static $NORTH_AMERICA = 'https://api-na.playground.klarna.com';
+    public static $OCEANIA = 'https://api-oc.playground.klarna.com';
+}
